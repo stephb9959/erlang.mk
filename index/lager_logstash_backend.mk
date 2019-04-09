d6c4ac73b23a7b8a259db79dfb4bdff09ba1cbe9
Stephane Bourque <stephane.bourque@gmail.com>
2019-04-08 22:45:37 -0700

    Add package lager_logstash_backend to the index

---
+++ index/lager_logstash_backend.mk
@@ -0,0 +1,7 @@
+PACKAGES += lager_logstash_backend
+pkg_lager_logstash_backend_name = lager_logstash_backend
+pkg_lager_logstash_backend_description = Lager logstash backend
+pkg_lager_logstash_backend_homepage = https://github.com/lambdaclass/lager_logstash_backend
+pkg_lager_logstash_backend_fetch = git
+pkg_lager_logstash_backend_repo = https://github.com/lambdaclass/lager_logstash_backend
+pkg_lager_logstash_backend_commit = 0.1.0
